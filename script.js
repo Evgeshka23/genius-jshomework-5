9fd80e8a237f2f56d571f720e528e30b3fcf0a29
Evgeshka23 <143422172+Evgeshka23@users.noreply.github.com>
2025-03-02 19:01:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Task: 1
+
+// class Abonent  {
+//   constructor(name, phone) {
+//     this.name = name;
+//     this.phone = phone;
+//   }
+// setName(name) {
+//     this.name = name;
+// }
+// getName() {
+//     return this.name;
+// }
+//   setPhone(phone) {
+//     this.phone = phone;
+//   }
+//   getPhone() {
+//     return this.phone;
+//   }
+// }
+
+// const abonent = new Abonent('Ivan', '1234567');
+// const abonent2 = new Abonent('Petr', '7654321');
+// const abonent3 = new Abonent('Vasya', '7654321');
